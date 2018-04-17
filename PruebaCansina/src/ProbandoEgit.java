7ccb1d09ccf88b4605ebfc20fbabf8db97f50540
Ana Perez <a2018@gmx.es>
2018-04-17 03:04:39 -0700

    Segunda Version

--- PruebaCansina/src/ProbandoEgit.java
+++ PruebaCansina/src/ProbandoEgit.java
@@ -4,6 +4,7 @@ public class ProbandoEgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("holam 1dam");
+		System.out.println("holam 1dam");
 
 	}
 
